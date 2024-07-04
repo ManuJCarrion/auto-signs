f25a635f41cc0a37055f37f899b5b0f389b5adbd
Manu J Carrión <60743938+ManuJCarrion@users.noreply.github.com>
2024-07-04 10:19:19 +0200

    Create utils.js

---
+++ src/utils.js
@@ -0,0 +1,55 @@
+const dayjs = require("dayjs");
+
+function buildAuthorizationHeader(accessToken) {
+  return { authorization: `Bearer ${accessToken}` };
+}
+
+function getTodayDate() {
+  return dayjs().format("YYYY-MM-DD");
+}
+
+function padLeft(value) {
+  if (value >= 10) {
+    return value.toString();
+  }
+
+  return `0${value}`;
+}
+
+function getSlotHour(slot) {
+  const todayDate = getTodayDate();
+  console.log(todayDate)
+
+  switch (slot) {
+    case 1: {
+      return `${todayDate}T${padLeft(process.env.START_HOUR)}:00:00+01:00`;
+    }
+    case 2: {
+      return `${todayDate}T${padLeft(process.env.BREAK_START_HOUR)}:00:00+01:00`;
+    }
+    case 3: {
+      return `${todayDate}T${padLeft(process.env.BREAK_END_HOUR)}:00:00+01:00`;
+    }
+    case 4: {
+      return `${todayDate}T${padLeft(process.env.END_HOUR)}:00:00+01:00`;
+    }
+  }
+}
+
+function buildSign(userId, slot) {
+  const date = getSlotHour(slot);
+
+  return {
+    DeviceId: "WebApp",
+    EndDate: date,
+    StartDate: date,
+    TimezoneOffset: -60,
+    UserId: userId,
+  };
+}
+
+module.exports = {
+  buildAuthorizationHeader,
+  getTodayDate,
+  buildSign,
+};
