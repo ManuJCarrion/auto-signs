9ad3b97ce0329cc7fb93c0b11ae32fc5d90f0495
Manu J Carrión <60743938+ManuJCarrion@users.noreply.github.com>
2024-07-05 10:21:05 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,127 +1,113 @@
-const dotenv = require("dotenv");
-const axios = require("axios").default;
-const qs = require("qs");
-const cron = require("node-cron");
-const express = require("express");
+const dotenv = require('dotenv');
+const axios = require('axios').default;
+const qs = require('qs');
+const cron = require('node-cron');
+const express = require('express');
 
-const utils = require("./utils");
+const utils = require('./utils');
 
-if (process.env.NODE_ENV === "development") {
-  dotenv.config();
+if (process.env.NODE_ENV === 'development') {
+    dotenv.config();
 }
 
-async function login(username, password) {
-  const body = { grant_type: "password", username, password };
+async function doLogin(username, password) {
+    const body = { grant_type: 'password', username, password };
 
-  return axios
-    .post(process.env.AUTH_URL, qs.stringify(body))
-    .then((res) => res.data.access_token);
+    return axios.post(process.env.AUTH_URL, qs.stringify(body)).then((res) => {
+        res.data.access_token, res.data.userName;
+    });
 }
 
-async function getUserId(accessToken) {
-  const headers = utils.buildAuthorizationHeader(accessToken);
+async function getIsHoliday(userId, accessToken) {
+    const headers = utils.buildAuthorizationHeader(accessToken);
 
-  return axios
-    .get(`${process.env.API_URL}/users`, { headers })
-    .then((res) => res.data.UserId);
+    return await axios
+        .get(`${process.env.API_URL}/users/${userId}/workdaylite`, { params, headers })
+        .then((res) => res.data.IsHoliday || res.data.IsWeekend);
 }
 
-async function getWorkingDay(userId, accessToken) {
-  const todayDate = utils.getTodayDate();
-  console.log(todayDate)
-  const headers = utils.buildAuthorizationHeader(accessToken);
-  const params = {
-    fromDate: todayDate,
-    toDate: todayDate,
-    pageIndex: 0,
-    pageSize: 1,
-  };
+async function postSign(accessToken) {
+    const headers = utils.buildAuthorizationHeader(accessToken);
+    const body = utils.buildSign();
 
-  return await axios
-    .get(`${process.env.API_URL}/users/${userId}/diaries/presence`, { params, headers })
-    .then((res) => res.data.Diaries[0]);
+    return await axios.post(`${process.env.API_URL}/svc/signs/signs`, body, { headers });
 }
 
-async function getSigns(accessToken) {
-  const headers = utils.buildAuthorizationHeader(accessToken);
+// async function main() {
+//     const { accessToken, userName } = await doLogin(process.env.USERNAME, process.env.PASSWORD);
+//     const isHoliday = await getIsHoliday(userName, accessToken);
 
-  return await axios
-    .get(`${process.env.API_URL}/signs`, { headers })
-    .then((res) => res.data);
-}
+//     if (isHoliday) {
+//         console.log('Hoy no ce trabaha shurras!');
+//         return;
+//     }
 
-async function postSign(userId, accessToken, slot) {
-  const headers = utils.buildAuthorizationHeader(accessToken);
-  const body = utils.buildSign(userId, slot);
+//     const signResponse = await postSign(accessToken);
 
-  return await axios.post(`${process.env.API_URL}/svc/signs/signs`, body, { headers });
+//     if (signResponse.status !== 201) {
+//         console.log(`Quillo ha pasao argo, esto no va`);
+//         return;
+//     }
+// }
+
+// para proposítos de demostración del cron
+async function postSignDemo(accessToken) {
+    const headers = utils.buildAuthorizationHeader(accessToken);
+    const body = utils.buildSign();
+
+    console.log('headers', headers);
+    console.log('body', body);
+
+    return { signEventId: 'a1a00ffd-6fb7-4910-8194-df8a3d811573' };
 }
 
 async function main() {
-  const accessToken = await login(process.env.USERNAME, process.env.PASSWORD);
-  // const userId = await getUserId(accessToken);
-  // const workingDay = await getWorkingDay(userId, accessToken);
+    const { accessToken, userName } = await doLogin(process.env.USERNAME, process.env.PASSWORD);
+    console.log('accessToken', accessToken);
+    console.log('userName', userName);
+    const isHoliday = await getIsHoliday(userName, accessToken);
+    console.log('isHoliday', isHoliday);
+    console.log('getTime', utils.getTime());
 
-  // if (workingDay.IsHoliday) {
-  //   console.log("Holi... day! You don't need to clock in/out today :)");
-  //   return;
-  // }
+    if (isHoliday) {
+        console.log('Hoy no ce trabaha shurras!');
+        return;
+    }
 
-  // const signs = await getSigns(accessToken);
+    const signResponse = await postSignDemo(accessToken);
 
-  // if (signs.length === 10) {
-  //   console.log("You have already clocked in/out for all of the today slots :D");
-  //   return;
-  // }
-
-  // const nextSlot = signs.length + 1;
-  // const signResponse = await postSign(userId, accessToken, nextSlot);
-
-  // if (signResponse.status !== 201) {
-  //   console.log(`There was an error clocking in/out for slot number: ${nextSlot}`);
-  //   return;
-  // }
-
-  console.log('accessToken', accessToken)
-
-  //console.log(`Clocked in/out successfully for slot number: ${nextSlot}`);
+    if (signResponse.status !== 201) {
+        console.log(`Quillo ha pasao argo, esto no va`);
+        return;
+    }
 }
 
 async function keepAlive() {
-  await axios.get(process.env.APP_URL);
+    await axios.get(process.env.APP_URL);
 }
 
 (async () => {
-  try {
-    const timezone = "Europe/Madrid";
-    const keepAliveCron = cron.schedule("*/15 * * * *", keepAlive, { timezone });
-    const signerCronHour = [
-      process.env.START_HOUR,
-      process.env.BREAK_START_HOUR,
-      process.env.BREAK_END_HOUR,
-      process.env.END_HOUR,
-    ]
-      .join(",")
-      .toString();
-    // const signerCron = new cron.schedule(`0 ${signerCronHour} * * 1-5`, main, {
-    //   timezone,
-    // });
-    const signerCron = new cron.schedule(`*/15 * * * *`, main, {
-      timezone,
-    });
-    const app = express();
+    try {
+        const timezone = 'Europe/Madrid';
+        const keepAliveCron = cron.schedule('*/15 * * * *', keepAlive, { timezone });
+        const signerCronHours = utils.getSignerCronHours();
+        console.log('signerCronHours', signerCronHours);
+        const signerCron = new cron.schedule(`0 ${signerCronHours} * * 1-5`, main, {
+            timezone,
+        });
 
-    keepAliveCron.start();
-    signerCron.start();
+        const app = express();
+        keepAliveCron.start();
+        signerCron.start();
 
-    app.get("/", (_, res) => {
-      res.send("The app would be alive for another 30min :D");
-    });
+        app.get('/', (_, res) => {
+            res.send('Esto va a seguí funsionando un rato máh!');
+        });
 
-    app.listen(process.env.PORT, async () => {
-      console.log(`App listening on port ${process.env.PORT}`);
-    });
-  } catch (err) {
-    console.error(err);
-  }
+        app.listen(process.env.PORT, async () => {
+            console.log(`App escuchando en el puerto ${process.env.PORT}`);
+        });
+    } catch (err) {
+        console.error(err);
+    }
 })();
